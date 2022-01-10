92469a2ee1b1696aebd312aba6da8fc6156dd013
Thobio Joseph <jthobio2@gmail.com>
2022-01-10 16:39:07 +0530

    Initial Commit

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Thobio Joseph on 10/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
